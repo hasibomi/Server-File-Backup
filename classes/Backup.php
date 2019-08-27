7e45839596d09cf959f515cbd40e7af96866102a
Hasibur Rahman Omi <hasib.omi@cimplux.com>
2019-08-27 21:49:14 +0600

    Getting, setting, deleting, searching directory done

--- classes/Backup.php
+++ classes/Backup.php
@@ -4,15 +4,105 @@ namespace Classes;
 
 class Backup
 {
-    private $directory;
+    private $file = __DIR__ . '/../server_file_backup_directory_list';
 
+    /**
+     * Set backup directory.
+     *
+     * @param $directory
+     */
     public function setDirectory($directory)
     {
-        $this->directory = $directory;
+        if (file_exists($this->file) && filesize($this->file) > 0) {
+            $size = filesize($this->file);
+
+            $file = fopen($this->file, 'r');
+            $contents = json_decode(fread($file, $size), true);
+            $filtered = array_filter($contents, function ($content) use ($directory) {
+                if ($content == $directory) {
+                    return $content;
+                }
+            });
+
+            if (count($filtered) > 0) {
+                die('Directory is already in list');
+            }
+        } else {
+            $contents = array();
+        }
+
+        $file = fopen($this->file, 'w');
+
+        array_push($contents, $directory);
+        fwrite($file, json_encode($contents));
     }
 
-    public function getDirectory()
+    /**
+     * Get all the directories for backup.
+     *
+     * @return array
+     */
+    public function getDirectories()
     {
-        return $this->directory;
+        if (file_exists($this->file) && filesize($this->file) > 0) {
+            $size = filesize($this->file);
+            $file = fopen($this->file, 'r');
+
+            return json_decode(fread($file, $size), true);
+        }
+
+        return array();
+    }
+
+    /**
+     * Search for the specified directory.
+     *
+     * @param $directory
+     * @return string
+     */
+    public function searchDirectory($directory)
+    {
+        $directories = $this->getDirectories();
+
+        if (count($directories) > 0) {
+            $result = array_filter($directories, function ($item) use ($directory) {
+                return $item == $directory;
+            });
+
+            if (count($result) > 0) {
+                $result = array_values($result);
+
+                return $result[0];
+            }
+        }
+
+        return 'Empty directory';
+    }
+
+    /**
+     * Delete the specified directory.
+     *
+     * @param $directory
+     * @return bool
+     */
+    public function deleteDirectory($directory)
+    {
+        $directories = $this->getDirectories();
+
+        if (count($directories) > 0) {
+            $results = array_filter($directories, function ($item) use ($directory) {
+                return $item != $directory;
+            });
+
+            if (count($results) > 0) {
+                $file = fopen($this->file, 'w');
+
+                fwrite($file, json_encode($results));
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
 
+require __DIR__ . '/classes/Backup.php';
+
+use Classes\Backup;
+
+$backup = new Backup();
+
+// $backup->setDirectory('/home/ubuntu');
+// echo join(', ', $backup->getDirectories());
+// echo $backup->searchDirectory('/home/ubuntu'), "\n";
+// print_r($backup->deleteDirectory('/home/ubuntu'));
