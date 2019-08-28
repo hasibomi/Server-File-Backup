e83a1868d7ebe02392beff66e84c3735b34e9dc6
Hasibur Rahman Omi <hasib.omi@cimplux.com>
2019-08-28 15:59:30 +0600

    Command line parser added

--- index.php
+++ index.php
@@ -6,9 +6,61 @@ use Classes\Backup;
 
 $backup = new Backup();
 
-// $backup->setDirectory('/Users/hasib.omi/Documents/Books/Accounting');
-// echo join(', ', $backup->getDirectories());
-// echo $backup->searchDirectory('/home/ubuntu'), "\n";
-// print_r($backup->deleteDirectory('/home/ubuntu'));
-// $backup->resetDirectories();
-// $backup->start();
+$commands = array(
+    'setdirectory',
+    'getdirectories',
+    'searchdirectory',
+    'removedirectory',
+    'resetdirectories',
+    'start',
+    '--help'
+);
+
+array_shift($argv);
+
+foreach ($argv as $arg) {
+    if (in_array($arg, $commands)) {
+        if ($arg === $commands[5]) {
+            $backup->start();
+            echo 'Done';
+        } else {
+            $key = array_search($arg, $argv);
+            $value = $argv[$key + 1];
+
+            switch ($arg) {
+                case $commands[0]:
+                    $backup->setDirectory($value);
+                    break;
+                case $commands[1]:
+                    echo join(",\n", $backup->getDirectories());
+                    break;
+                case $commands[2]:
+                    echo $backup->searchDirectory($value);
+                    break;
+                case $commands[3]:
+                    if ($backup->deleteDirectory($value)) {
+                        echo 'Directory removed';
+                    } else {
+                        echo 'Could not find the directory';
+                    }
+
+                    break;
+                case $commands[4]:
+                    $backup->resetDirectories();
+                    echo 'Directory list reset';
+                    break;
+                default:
+                    show_available_commands($commands);
+            }
+        }
+    } else {
+        show_available_commands($commands);
+    }
+
+    echo "\n";
+}
+
+function show_available_commands($commands) {
+    echo 'Available commands are:', "\n";
+    echo join(",\n", $commands);
+}
